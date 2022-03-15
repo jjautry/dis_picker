49290ab1af598619cce634b7f59249dd9419a589
jjautry <52764049+jjautry@users.noreply.github.com>
2022-03-14 22:46:57 -0400

    Complee movie function
    - complete movie function
    - add movie poster art to result
    - updated database to store links to posters

--- dis_picker4web.py
+++ dis_picker4web.py
@@ -92,29 +92,35 @@ def logout():
     return redirect('/')
 
 
-# @app.route('/movie/<movie_name>')
-# def movie(movie_name):
-#     """This is outdated and will be deleted"""
-#     if movie_name in movie_dict:
-#         name = movie_dict[movie_name]['title']
-#         studio = movie_dict[movie_name]['studio']
-#         year = movie_dict[movie_name]['year released']
-#         category = movie_dict[movie_name]['category']
-#         logo_url = ""
-#         if studio == "Disney":
-#             logo_url = "../static/The_Walt_Disney_Studios_logo.png"
-#         elif studio == "Disney Channel":
-#             logo_url = "../static/disneychannel.png"
-#         elif studio == "Marvel":
-#             logo_url = "../static/marvel.png"
-#         elif studio == "Pixar":
-#             logo_url = "../static/pixar.png"
-#         elif studio == "Lucasfilm":
-#             logo_url = "../static/lucasfilm.png"
-#
-#     return render_template('movie.html', temp_name=name,
-#                            temp_studio=studio, temp_year=year,
-#                            temp_cat = category, temp_logo_url=logo_url)
+@app.route("/studio")
+def studio():
+    return render_template("studio.html")
+
+
+@app.route("/marvel-phase")
+def marvel():
+    query = "SELECT * FROM movies WHERE production_company='Marvel' AND phase=1 ORDER BY RANDOM() LIMIT 1;"
+    return render_template("marvel_phase.html")
+
+
+@app.route("/studio/<studio>")
+def year(studio):
+    new_studio = str(studio)
+    if new_studio == 'Lucasfilm':
+        query = "SELECT * FROM movies WHERE production_company='Lucasfilm' ORDER BY RANDOM() LIMIT 1;"
+    elif new_studio == 'Disney':
+        query = "SELECT * FROM movies WHERE production_company='Disney' ORDER BY RANDOM() LIMIT 1;"
+    elif new_studio == 'Disney Channel':
+        query = "SELECT * FROM movies WHERE production_company='Disney Channel' ORDER BY RANDOM() LIMIT 1;"
+    elif new_studio == 'Pixar':
+        query = "SELECT * FROM movies WHERE production_company='Pixar' ORDER BY RANDOM() LIMIT 1;"
+    elif new_studio == 'Marvel':
+        query = "SELECT * FROM movies WHERE production_company='Marvel' ORDER BY RANDOM() LIMIT 1;"
+    cursor = DBConnect().cursor
+    cursor.execute(query)
+    result = cursor.fetchall()
+    return render_template("movie.html", result=result)
+
 
 
 if __name__ == '__main__':
--- movie_selector.py
+++ movie_selector.py
@@ -2,8 +2,8 @@ from datetime import datetime
 
 
 def dis_countdown():
-   """Takes current date and returns days left until Disney trip"""
-   today = datetime.today().date()
-   disney_date = datetime(2022, 10, 9).date()
-   delta = disney_date - today
-   return delta.days
+    """Takes current date and returns days left until Disney trip"""
+    today = datetime.today().date()
+    disney_date = datetime(2022, 10, 9).date()
+    delta = disney_date - today
+    return delta.days
