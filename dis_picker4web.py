92e297fec68281bd1f930674cdc58a828f6e7028
jjautry <52764049+jjautry@users.noreply.github.com>
2022-03-19 21:17:59 -0400

    begin work on home page overhaul
    - update bootstrap dependencies
    - implement grid layout
    - new dynamic home page logo(s)

--- dis_picker4web.py
+++ dis_picker4web.py
@@ -21,7 +21,7 @@ def create_table():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index-2.html')
 
 
 @app.route('/login', methods=['POST', 'GET'])
